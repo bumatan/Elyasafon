c43848d33d3964ab0814d02776fa5846d1cd23b5
Matan Ben Yehuda <bumatan1@gmail.com>
2020-09-22 19:21:00 +0300

    test

--- connection_controller.py
+++ connection_controller.py
@@ -17,7 +17,6 @@ class connection_controller(object):
 
         # Walk on all the bin values and set the correct index
         pins_values = bin(index-1)[2:].zfill(3)
-        print(pins_values)
         for index in range(len(self.index_pins)):
             if (pins_values[index] == '1'):
                 GPIO.output(self.index_pins[index], GPIO.HIGH)
